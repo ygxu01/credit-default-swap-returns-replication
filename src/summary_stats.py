2e7b38d94878e32c301bfaaa7184f3d71dda0294
ygxu01 <yanggexu@uchicago.edu>
2025-03-18 10:21:58 -0500

    summary_stats.py

--- src/summary_stats.py
+++ src/summary_stats.py
@@ -26,6 +26,8 @@ cds_return = cds_return.merge(sector, on='ticker', how='left')
 monthly_return = calc_cds_monthly_return(create_yyyymm_col(cds_return))
 monthly_return = monthly_return.merge(sector, on='ticker', how='left')
 sector_describe = monthly_return.groupby("sector")["daily_return"].describe()
+sector_describe["count"] = sector_describe["count"].astype(int)
+sector_describe = sector_describe.rename(columns={"25%": "0.25", "50%": "0.5", "75%": "0.75"})
 
 latex_table_string = sector_describe.to_latex(float_format=float_format_func)
 print(latex_table_string)
@@ -35,14 +37,18 @@ with open(path, "w") as text_file:
     text_file.write(latex_table_string)
 
 ## Graph1
+monthly_return["yyyymm"] = monthly_return["yyyymm"].apply(month_code_to_date)
+monthly_grouped = monthly_return.groupby(["sector","yyyymm"])["daily_return"].mean().reset_index()
 plt.figure(figsize=(12, 6))
-sns.lineplot(data=monthly_return, x="yyyymm", y="daily_return", marker="o", alpha=0.7)
+sns.lineplot(data=monthly_grouped, x="yyyymm", y="daily_return", hue="sector", alpha=0.7)
 
-plt.title("Monthly CDS Portfolio Returns Over Time", fontsize=14)
 plt.xlabel("Time (YYYY-MM)", fontsize=12)
 plt.ylabel("Average Monthly Return", fontsize=12)
 plt.xticks(rotation=45)
 plt.grid(True)
+
+# Move legend outside the plot for better visibility
+plt.legend(title="Sector", bbox_to_anchor=(1.05, 1), loc='upper left')
 
 # Save plot as PNG
 output_plot_path = OUTPUT_DIR / "monthly_returns_over_time.png"
@@ -50,3 +56,16 @@ plt.savefig(output_plot_path, dpi=300, bbox_inches="tight")
 plt.close()
 
 print(f"Saved plot to: {output_plot_path}")
+## Graph2
+plt.figure(figsize=(12, 6))
+sns.boxplot(data=monthly_grouped, x="sector", y="daily_return")
+
+plt.xlabel("Sector", fontsize=12)
+plt.ylabel("Monthly Return", fontsize=12)
+plt.xticks(rotation=45)
+
+# Save the boxplot
+boxplot_path = OUTPUT_DIR / "boxplot_cds_returns_by_sector.png"
+plt.savefig(boxplot_path, dpi=300, bbox_inches="tight")
+plt.close()
+print(f"Saved plot to: {boxplot_path}")
